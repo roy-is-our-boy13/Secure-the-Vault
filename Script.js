3ed2dd244371cc39f2ee233aa76b15eb0fb7ac8c
Roy <rjperlman@gmail.com>
2024-11-04 10:19:51 -0800

    fixed some comments grammars

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-//Pseudocoe: Create a string called message to give the user a message.
+//Pseudocode: Create a string called message to give the user a message.
 /*  This message varivale with store this 
     message "You have received this message 
     because you have been chosen to open an 
@@ -6,24 +6,24 @@
     combination:"*/
 let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 
-// Pseudocoe: 5 * 2 = 10
+// Pseudocode: 5 * 2 = 10
 // Let num1 be eqaul to 10 when 5 is multiplied by 2.
 const num1 = 5 * 2;
 
-// Pseudocoe: 39 + 1 = 40
+// Pseudocode: 39 + 1 = 40
 // Let num2 be eqaul to 40 when 39 is being added by 1.
 const num2 = 39 + 1;
 
-// Pseudocoe: 78/2 = 39
+// Pseudocode: 78/2 = 39
 // Let num3 be eqaul to 39 when 78 is being divied by 2.
 const num3 = 78/2;
 
-// Pseudocoe: mainFullMessage = message + "-" + num1 + "-" + num2 + "-" +num3
+// Pseudocode: mainFullMessage = message + "-" + num1 + "-" + num2 + "-" +num3
 // This will be the main message with all the varaibles combinded. 
 const mainFullMessage = `${message} ${num1}-${num2}-${num3}`;
 
 /* 
-Pseudocoe: Create a dialog box that should say "You have received this message 
+Pseudocode: Create a dialog box that should say "You have received this message 
     because you have been chosen to open an 
     important vault. Here is the secret 
     combination: 10-40-39
