3e377a32a68a10b85a0d7614cb756908804fca1f
Roy <rjperlman@gmail.com>
2024-11-04 09:36:14 -0800

    Add files

---
+++ Script.js
@@ -0,0 +1,26 @@
+
+/*  This message varivale with store this 
+    message "You have received this message 
+    because you have been chosen to open an 
+    important vault. Here is the secret 
+    combination:"*/
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Let num1 be eqaul to 10 when 5 is multiplied by 2.
+// 5 * 2 = 10
+const num1 = 5 * 2;
+
+// Let num2 be eqaul to 40 when 39 is being added by 1.
+// 39 + 1 = 40
+const num2 = 39 + 1;
+
+// Let num3 be eqaul to 39 when 78 is being divied by 2.
+// 78/2 = 39
+const num3 = 78/2;
+
+// This will be the main message with all the varaibles combinded. 
+const mainFullMesage = `${message} ${num1}-${num2}-${num3}`;
+
+// This will display the main the full message when values 'message', 'num1', 'num2', and 'num3' are all combined. 
+alert(`${message} ${num1}-${num2}-${num3}`);
+console.log(mainFullMesage);
